b2782196c50f9d082fa16f91c6dc3e2cf7e67f54
Nawalhossam <149715233+Nawalhossam@users.noreply.github.com>
2024-12-07 22:08:55 +0200

    Draw ball (#2)
    * Draw_Ball
    
    * Draw_Ball_Proc
    
    * Modify in Proc
    
    * Add_Main_Ball
    
    * Modify
    
    * Edit
    
    * Delete mainBall

---
+++ Draw_Ball.asm
@@ -0,0 +1,49 @@
+EXTRN Ball_X:WORD
+EXTRN Ball_Y:WORD
+EXTRN Ball_Pixels:WORD
+
+
+PUBLIC Draw_Ball
+.MODEL SMALL
+.STACK 100h
+
+.CODE
+Draw_Ball PROC FAR
+
+
+  
+    MOV AH,00h  ;Move to graphic mode
+    MOV AL,13h
+    INT 10h
+
+
+    MOV CX,Ball_X   ;Mov the x,y for ball
+    MOV DX,Ball_Y
+    MOV AX,00h
+
+
+
+
+    DRAW_HORIZONTAL_VERTICAL:
+    MOV AH,0ch
+    MOV AL,0Ah
+    MOV BH,00h
+    INT 10h    ; Draw the pixel
+    INC CX
+    MOV AX,CX
+    SUB AX,Ball_X
+    CMP AX,Ball_Pixels
+    JNG DRAW_HORIZONTAL_VERTICAL   ;Loop for X-axis
+
+
+
+    MOV CX,Ball_X
+    INC DX
+    MOV AX,DX
+    SUB AX,Ball_Y
+    CMP AX,Ball_Pixels
+    JNG DRAW_HORIZONTAL_VERTICAL   ;Loop for Y-axis
+
+
+ Draw_Ball ENDP
+ END
