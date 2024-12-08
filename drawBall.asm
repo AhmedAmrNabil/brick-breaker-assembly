a651acd515bed1c653c293c0f590712ba6749348
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-08 19:38:19 +0200

    feat: add clearBall proc

--- drawBall.asm
+++ drawBall.asm
@@ -34,4 +34,30 @@ drawBall PROC FAR
     CMP AX,BALL_PIXELS
     JNG DRAW_HORIZONTAL_VERTICAL   ;Loop for Y-axis
 drawBall ENDP
+
+clearBall PROC FAR
+    MOV AX,00h
+
+    MOV CX,Ball_X   ;Mov the x,y for ball
+    MOV DX,Ball_Y
+    MOV AX,00h
+
+    CLEAR_BALL_HORIZONTAL_VERTICAL:
+    MOV AH,0ch
+    MOV AL,00h
+    MOV BH,00h
+    INT 10h    ; Clear the pixel
+    INC CX
+    MOV AX,CX
+    SUB AX,Ball_X
+    CMP AX,BALL_PIXELS
+    JNG CLEAR_BALL_HORIZONTAL_VERTICAL   ;Loop for X-axis
+
+    MOV CX,Ball_X
+    INC DX
+    MOV AX,DX
+    SUB AX,Ball_Y
+    CMP AX,BALL_PIXELS
+    JNG CLEAR_BALL_HORIZONTAL_VERTICAL   ;Loop for Y-axis
+clearBall ENDP
 END
