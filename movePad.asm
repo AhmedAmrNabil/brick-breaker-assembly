004fb97f6711feff34eca45659788e54bf4071c2
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-12-14 21:10:01 +0200

    feat: implement dual paddle movement with separate procedures

--- movePad.asm
+++ movePad.asm
@@ -1,30 +1,32 @@
+EXTRN PADDLE_X:WORD
 EXTRN PADDLE1_X:WORD
 EXTRN PADDLE1_VEL_X:WORD
-EXTRN PADDLE_X:WORD
+EXTRN PADDLE2_X:WORD
+EXTRN PADDLE2_VEL_X:WORD
 EXTRN drawPaddle:FAR
 EXTRN clearPaddle:FAR
 
-PUBLIC movePaddle
-
+PUBLIC movePaddle1
+PUBLIC movePaddle2
 
 .MODEL small
 .386
 
 .DATA
-PADDLE_WIDTH EQU 50
+	PADDLE_WIDTH EQU 50
 
 .CODE
-movePaddle PROC FAR
+movePaddle1 PROC FAR
 
-	cmp PADDLE1_VEL_X,0
-	JE skipMovePaddle
+	CMP PADDLE1_VEL_X,0
+	JE skipMovePaddle1
 
-	mov ax, PADDLE1_X
+	MOV AX, PADDLE1_X
 	ADD AX, PADDLE1_VEL_X
-	CMP AX,0
-	JL skipMovePaddle
-	CMP AX,160 - PADDLE_WIDTH
-	JG skipMovePaddle
+	CMP AX, 0
+	JL skipMovePaddle1
+	CMP AX, 160 - PADDLE_WIDTH
+	JG skipMovePaddle1
 
 	MOV AX, PADDLE1_X
 	MOV PADDLE_X, AX
@@ -32,13 +34,39 @@ movePaddle PROC FAR
 
 	MOV AX, PADDLE1_X
 	ADD AX, PADDLE1_VEL_X
-
 	MOV PADDLE1_X, AX
 	MOV PADDLE_X, AX
 	CALL drawPaddle
 
-skipMovePaddle:
+skipMovePaddle1:
 	RET
 
-movePaddle ENDP
+movePaddle1 ENDP
+
+movePaddle2 PROC FAR
+
+	CMP PADDLE2_VEL_X,0
+	JE skipMovePaddle2
+
+	MOV AX, PADDLE2_X
+	ADD AX, PADDLE2_VEL_X
+	CMP AX, 0
+	JL skipMovePaddle2
+	CMP AX, 160 - PADDLE_WIDTH
+	JG skipMovePaddle2
+
+	MOV AX, PADDLE2_X
+	MOV PADDLE_X, AX
+	CALL clearPaddle
+
+	MOV AX, PADDLE2_X
+	ADD AX, PADDLE2_VEL_X
+	MOV PADDLE2_X, AX
+	MOV PADDLE_X, AX
+	CALL drawPaddle
+
+skipMovePaddle2:
+	RET
+
+movePaddle2 ENDP
 END
