383ab654a0fefbd512f8f1328ac4ab1202617c48
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-12-24 01:43:24 +0200

    feat: add colored tile proc

--- drawTile.asm
+++ drawTile.asm
@@ -1,6 +1,7 @@
 EXTRN CLEAR_TILE_OFFSET:BYTE
 
 PUBLIC drawTile
+PUBLIC drawColoredTile
 PUBLIC clearTile
 
 .MODEL small
@@ -16,8 +17,12 @@ PUBLIC clearTile
 	BRICK_COLOR equ 20d
 	BORDER_COLOR_1 equ 18d
 	BORDER_COLOR_2 equ 22d
+	COLORED_BRICK_COLOR equ 44d
+	COLORED_BORDER_COLOR_1 equ 140d
+	COLORED_BORDER_COLOR_2 equ 92d
 
 .CODE
+
 drawTile PROC FAR
 	PUSHA
 	PUSHF
@@ -105,6 +110,98 @@ drawBotBorder:
 	POPA
 	RET
 drawTile ENDP
+
+
+drawColoredTile PROC FAR
+	PUSHA
+	PUSHF
+
+	MOV BX, AX    	; BH = y, BL = x
+	MOV AX, 0
+
+	; get the x pixel coordinate
+	MOV AL, BL
+	MOV DL, TILE_WIDTH
+	MUL DL
+	MOV TILE_START_X, AX
+	ADD AX, TILE_WIDTH
+	MOV TILE_END_X, AX
+
+	MOV AX, 0
+
+	; get the y pixel coordinate
+	MOV AL, BH
+	MOV DL, TILE_HEIGHT
+	MUL DL
+	MOV TILE_START_Y, AX
+	ADD AX, TILE_HEIGHT
+	MOV TITLE_END_Y, AX
+
+	; draw the tile
+	MOV AL, COLORED_BRICK_COLOR
+	MOV AH, 0ch
+	MOV CX, TILE_START_X
+	MOV DX, TILE_START_Y
+drawColoredX:
+	INT 10h
+	INC DX
+	CMP DX, TITLE_END_Y
+	JL drawColoredX
+	MOV DX, TILE_START_Y
+	INC CX
+	CMP CX, TILE_END_X
+	JL drawColoredX
+
+	; draw borders
+	MOV AL, COLORED_BORDER_COLOR_2
+	MOV AH, 0ch
+	MOV CX, TILE_START_X
+	MOV DX, TILE_START_Y
+drawColoredLeftBorder:
+	INT 10h
+	INC DX
+	CMP DX, TITLE_END_Y
+	JL drawColoredLeftBorder
+
+	MOV AL, COLORED_BORDER_COLOR_1
+	MOV AH, 0ch
+	MOV CX, TILE_END_X
+	DEC CX
+	MOV DX, TILE_START_Y
+drawColoredRightBorder:
+	INT 10h
+	INC DX
+	CMP DX, TITLE_END_Y
+	JL drawColoredRightBorder
+
+	MOV AL, COLORED_BORDER_COLOR_2
+	MOV AH, 0ch
+	MOV CX, TILE_START_X
+	MOV DX, TILE_START_Y
+drawColoredTopBorder:
+	INT 10h
+	INC CX
+	CMP CX, TILE_END_X
+	JL drawColoredTopBorder
+
+	MOV AL, COLORED_BORDER_COLOR_1
+	MOV AH, 0ch
+	MOV CX, TILE_START_X
+	MOV DX, TITLE_END_Y
+	DEC DX
+drawColoredBotBorder:
+	INT 10h
+	INC CX
+	CMP CX, TILE_END_X
+	JL drawColoredBotBorder
+
+	POPF
+	POPA
+	RET
+drawColoredTile ENDP
+
+
+
 
 clearTile PROC FAR
 	PUSHA
